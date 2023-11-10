1573f56b1fed5a8d6866a42f1606d034c0be2211
Phu Le <21522466@gm.uit.edu.vn>
2023-11-10 22:53:34 +0700

    app frame

---
+++ Billiard4Life_CNPMCS/ViewModel/LichSuBanViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billiard4Life_CNPMCS.ViewModel
+{
+    public class LichSuBanViewModel : BaseViewModel
+    {
+    }
+}
